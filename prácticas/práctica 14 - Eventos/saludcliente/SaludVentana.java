2c8aab212c1f35bb052480325b86665c903dfc75
servkey <luis.montane@hotmail.com>
2015-11-11 11:15:23 -0600

    GUI - Eventos

--- prácticas/práctica 14 - Eventos/saludcliente/SaludVentana.java
+++ prácticas/práctica 14 - Eventos/saludcliente/SaludVentana.java
@@ -2,11 +2,11 @@ package saludcliente;
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
-
+import salud.*;
 
 public class SaludVentana extends JFrame{
 	private JButton btnCalcularIMC, btnCalcularPG;
-	private JLabel lblNombre, lblEdad, lblSexo, lblEstatura, lblPeso, lblCintura, lblCuello, lblCadera;
+	private JLabel lblNombre, lblEdad, lblSexo, lblEstatura, lblPeso, lblCintura, lblCuello, lblCadera, lblIMC, lblPG, lblIMCValor, lblPGValor;
 	private JTextField txtNombre, txtEdad, txtSexo, txtEstatura, txtPeso, txtCintura, txtCuello, txtCadera;
 
 	private void inicializarComponentes(){
@@ -21,6 +21,10 @@ public class SaludVentana extends JFrame{
 		lblCintura = new JLabel("Cintura");
 		lblCuello = new JLabel("Cuello");
 		lblCadera = new JLabel("Cadera");
+		lblIMC = new JLabel("IMC");
+		lblIMCValor = new JLabel();
+		lblPG = new JLabel("PG");
+		lblPGValor = new JLabel();
 		
 		txtNombre = new JTextField();
 		txtEdad = new JTextField();
@@ -48,33 +52,60 @@ public class SaludVentana extends JFrame{
 		add(lblCadera);
 		add(txtCadera);
 
+		add(lblIMC);
+		add(lblIMCValor);
+	
+		add(lblPG);
+		add(lblPGValor);
+
 		add(btnCalcularIMC);
 		add(btnCalcularPG);
-
-
 
 		btnCalcularIMC.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				System.out.println("Click Botón 1 - Calculando IMC...");
-				System.out.println(txtNombre.getText());
+				Persona p = obtenerDatos();
+				System.out.println("IMC: " + p.indMasaCor());	
+				lblIMCValor.setText("" + p.indMasaCor());
 			}
 		});
 
 		btnCalcularPG.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				System.out.println("Click Botón 1 - Calculando PG...");
+				Persona p = obtenerDatos();
+				System.out.println("PG: " + p.porGrasa());	
+				lblPGValor.setText(""+ p.porGrasa());
 			}
 		});
 	}
 
-	private void obtenerDatos(){
+	private Persona obtenerDatos(){
+		Persona p = null;
 		
+		String sexo = txtSexo.getText();
+		String nombre = txtNombre.getText();
+		int edad = Integer.parseInt(txtEdad.getText());
+		float estatura = Float.parseFloat(txtEstatura.getText());
+		float peso = Float.parseFloat(txtPeso.getText());
+		float cintura = Float.parseFloat(txtCintura.getText());
+		float cuello = Float.parseFloat(txtCuello.getText());				
+		java.util.Date fechaNacimiento = new java.util.Date("10/10/1980");
+		if (sexo.equals("M")){
+			//Mujer
+			float cadera = Float.parseFloat(txtCadera.getText());				
+			p = new Mujer(nombre, edad, peso, estatura, fechaNacimiento, cintura, cuello, cadera); 
+		}else if (sexo.equals("H")){
+			//Hombre
+			p = new Persona(nombre, edad, peso, estatura, fechaNacimiento, cintura, cuello);	
+		}
+		return p;
 	}
 
 
 	public SaludVentana(){
 		super("Cliente para calcular IMC, PG");
-		setLayout(new GridLayout(9,2));
+		setLayout(new GridLayout(11,2));
 		
 		setSize(400, 300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
