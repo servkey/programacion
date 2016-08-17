76364932fbae03f8c6044b4167bd55f01aa2ba42
servkey <luis.montane@hotmail.com>
2016-08-16 23:08:52 -0500

    Práctica 0 Biblioteca

---
+++ prácticas/práctica 1 - Biblioteca/Biblioteca.java
@@ -0,0 +1,23 @@
+public class Biblioteca{
+	public static void main(String args[]){
+
+		//Construir Persona1 
+
+		Persona p1 =  new Persona();
+		System.out.println("**Información de Persona****");
+		p1.setNombre("Luis");
+		p1.setEmail("luis@gmail.com");
+
+		System.out.println(p11);		
+		//System.out.println("Representación Cadena:" + p1.toString());
+		
+		System.out.println(p1.getNombre());
+		System.out.println(p1.getEmail());
+		
+		/*int x = 10;
+		String nombre = "Luis";
+
+		System.out.println("X = " + x);
+		System.out.println("Nombre = " + nombre);*/
+	}
+}
